53936026d9a7e45050f00ded5dc39c08b413f17e
João Franco <joao_pp_franco@estudante.sesisenai.org.br>
2025-04-04 09:29:11 -0300

    menu corrigido

--- quimica para todos/menu/menu.js
+++ quimica para todos/menu/menu.js
@@ -1,23 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/menu/menu.html")  // Caminho para o arquivo menu.html
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML("afterbegin", data); // Insere o menu no topo do <body>
-            carregarMenu(); // Chama a função para ativar os eventos do menu
-        })
-        .catch(error => console.error("Erro ao carregar o menu:", error));
-});
-
-function carregarMenu() {
     const menuBtn = document.getElementById("menu-btn");
     const sidebar = document.querySelector(".sidebar");
-    let overlay = document.querySelector(".overlay");
-
-    if (!overlay) {
-        overlay = document.createElement("div");
-        overlay.classList.add("overlay");
-        document.body.appendChild(overlay);
-    }
+    const overlay = document.getElementById("overlay");
 
     menuBtn.addEventListener("click", function () {
         sidebar.classList.toggle("active");
@@ -28,4 +12,4 @@ function carregarMenu() {
         sidebar.classList.remove("active");
         overlay.classList.remove("active");
     });
-}
+});
